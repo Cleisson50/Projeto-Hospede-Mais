45e8064e7fe1f3f97dbe54f76bbef385c6d6051f
Cleisson <62613949+Cleisson50@users.noreply.github.com>
2022-07-14 14:18:07 -0300

    Adicionando o login com google

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -25,11 +25,35 @@ export default function Login({ navigation }) {
                 let errorMessage = error.message;
             });
     }
+    const loginGoogle = () => {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                /** @type {firebase.auth.OAuthCredential} */
+                var credential = result.credential;
+
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = credential.accessToken;
+                // The signed-in user info.
+                var user = result.user;
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+            });
+    }
 
     useEffect(() => {
         firebase.auth().onAuthStateChanged((user) => {
             if (user) {
-                navigation.navigate("Task", {idUser: user.uid})
+                navigation.navigate("Task", { idUser: user.uid })
             }
         });
     }, []);
@@ -62,6 +86,9 @@ export default function Login({ navigation }) {
                     <Text style={styles.textButtonLogin}>Login</Text>
                 </TouchableOpacity>
             }
+            <TouchableOpacity style={styles.buttonLogin} onPress={loginGoogle}>
+                <Text style={styles.textButtonLogin}>Login com Google</Text>
+            </TouchableOpacity>
             <Text style={styles.registration}>
                 Não está registrado?
                 <Text style={styles.linkSubscribe} onPress={() => navigation.navigate("NewUser")}>
