7fa9cb6ca4701ec67b0696aa31259a8cbd8a1184
Cleisson <62613949+Cleisson50@users.noreply.github.com>
2022-07-17 16:34:02 -0300

    Adicionando os campos de Nome e Telefone

--- src/pages/NewUser/index.js
+++ src/pages/NewUser/index.js
@@ -1,37 +1,45 @@
 import React, { useState } from "react";
 import { View, Text, KeyboardAvoidingView, TextInput, TouchableOpacity } from "react-native";
 import styles from "./styles";
+import { collection, getDocs } from "firebase/firestore";
 import firebase from "../../config/firebaseConfig";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 
 export default function NewUser({ navigation }) {
+    const database = firebase.firestore()
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
     const [nome, setNome] = useState("");
+    const [telefone, setTelefone] = useState("")
     const [errorRegister, setErrorRegister] = useState("");
 
     const register = () => {
         firebase.auth().createUserWithEmailAndPassword(email, senha)
-            .then((userCredential) => {
-                // Signed in
-                let user = userCredential.user;
-                navigation.navigate("Task", { idUser: user.uid })
-                // ...
-            })
-            .catch((error) => {
-                setErrorRegister(true)
-                let errorCode = error.code;
-                let errorMessage = error.message;
-                // ..
+        .then(database => {
+            // Signed in
+            const uid = database.user.uid;
+            const users = firebase.firestore().collection('users');
+            users.doc(uid).set({
+                email: email, name: nome, telefone: telefone
             });
+            navigation.navigate("Task", { idUser: user.uid })
+            // ...
+        })
+        .catch((error) => {
+            setErrorRegister(true)
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            // ..
+        });
     }
 
     return (
         <KeyboardAvoidingView behavior="padding" style={styles.container}>
             <Text style={styles.title}>Criar uma nova conta</Text>
-            {/* <TextInput style={styles.input} placeholder="Insira seu nome" type="text" onChangeText={(text) => setNome(text)} value={nome} /> */}
+            <TextInput style={styles.input} placeholder="Insira seu nome" type="text" onChangeText={(text) => setNome(text)} value={nome} />
             <TextInput style={styles.input} placeholder="Insira seu email" type="text" onChangeText={(text) => setEmail(text)} value={email} />
+            <TextInput style={styles.input} autoComplete="tel-country-code" maxLength={18} keyboardType="phone-pad" placeholder="Insira seu número" type="text" onChangeText={(text) => setTelefone(text)} value={telefone} />
             <TextInput style={styles.input} secureTextEntry={true} placeholder="Insira uma senha" type="text" onChangeText={(text) => setSenha(text)} value={senha} />
             {errorRegister === true
                 ?
