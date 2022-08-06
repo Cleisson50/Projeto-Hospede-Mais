02e29a109fa10bac361b9b3c16f4c9de3be30a04
Cleisson <62613949+Cleisson50@users.noreply.github.com>
2022-08-06 18:23:02 -0300

    Ajustando a visualização do nome do usuario do banco

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,9 +1,9 @@
 import renderer from 'react-test-renderer';
 import App from '../App';
+import { fireEvent, render } from '@testing-library/react-native';
+jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
 
-describe('Teste de Snapshot', () => {
-    it('Snapshot de App', () => {
-        const tela = renderer.create(<App />).toJSON();
-        expect(tela).toMatchSnapshot();
-    })
-})
+test('teste de snapshot', () => {
+    const tela = renderer.create(<App />).toJSON();
+    expect(tela).toMatchSnapshot();
+});
